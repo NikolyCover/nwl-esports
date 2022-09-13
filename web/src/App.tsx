8ae675dfe4c892e62c10b15c1d3ef01c7ab7aa52
Nikoly Cover Pereira <nikoly.cp@pti.org.br>
2022-09-13 11:50:27 -0300

    building index page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,79 @@
 import './styles/main.css'
+import logoImg from './assets/logo-nwl-esports.svg'
 
 function App() {
   return (
-    <div className='w'></div>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center m-20'>
+      <img src={logoImg} ></img>
+
+      <h1 className='text-6xl text-white font-black m-20'>
+        Seu <span className='text-transparent bg-nwl-gradient bg-clip-text'>duo</span> está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-1.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-2.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-3.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CSGO</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-4.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-5.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-6.png' alt=''/>
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className='mt-8 pt-1 bg-nwl-gradient self-stretch rounded-lg overflow-hidden '>
+        <div className='bg-[#2A2634] px-8 py-6 self-stretch flex justify-between'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400'>Publique um anúncion para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded'>
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
